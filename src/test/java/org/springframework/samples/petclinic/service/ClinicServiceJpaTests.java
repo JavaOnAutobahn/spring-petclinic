208f665e17f3d8650b82f1169bbb5fab7e67807b
Markus Harrer <feststelltaste@googlemail.com>
2018-11-15 18:39:27 +0100

    add direct file import with callouts

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceJpaTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceJpaTests.java
@@ -14,9 +14,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @see AbstractClinicServiceTests AbstractClinicServiceTests for more details. </p>
  */
 
+// tag::test_annotations[]
 @ContextConfiguration(locations = {"classpath:spring/business-config.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("jpa")
+@ActiveProfiles("jpa") // <1>
 public class ClinicServiceJpaTests extends AbstractClinicServiceTests {
-
+// end::test_annotations[]
 }
